1ddb91052528f82e51a2f4cf6264fd77e3c37386
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-03-18 10:28:51 +0000

    Changes in the communications package. Working with Java RMI,  dont know if its the best idea but at least we have a possible structure.

---
+++ CakeChuff/src/cc/communications/CommunicationInterface.java
@@ -0,0 +1,17 @@
+package cc.communications;
+
+public interface CommunicationInterface {
+
+	public abstract Mailbox getMailbox(int code);
+
+	public abstract boolean getSent(int code);
+
+	public abstract boolean getReceived(int code);
+
+	public abstract void receive(int code);
+
+	public abstract void send(int code,Packet output);
+
+	public abstract void sendReal(int code);
+
+}
---
+++ CakeChuff/src/cc/communications/ServerInterface.java
@@ -0,0 +1,17 @@
+package cc.communications;
+
+import java.rmi.*;
+
+
+	public interface ServerInterface extends Remote{
+
+
+		 public void registerClient(int code, ClientInterface object) throws RemoteException;
+
+		 public void receiveReal(int code, Packet input)throws java.rmi.RemoteException;
+
+			 	 
+
+	}
+
+
