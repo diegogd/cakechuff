ed8d7d04808f1f47b13ce200c48fe96825ccb4b1
selvaya <selvaya@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-04-06 19:28:33 +0000

    Added xml, and dtd as the database for cakechuff. Added XMLManager, using jdom to read and write the xml. Modify SCADA.java with get/set methods to simplify the access to xml files. It needs some checks but it is more or less ok.

--- trunk/CakeChuff/src/cc/scada/SCADA.java
+++ trunk/CakeChuff/src/cc/scada/SCADA.java
@@ -1,5 +1,105 @@
 package cc.scada;
+import java.io.*;
+import java.util.*;
 
 public class SCADA {
+	
+
+	public SCADA (){
+
+	}
+	
+	/**
+	 * This method is to recover the information of the subsystems.
+	 * @param key The name of the value in the xml file that we want to 
+	 * recover. This can be:
+	 *          defaultSpeed
+	 *          conveyorSpeed
+	 *          defaultv1
+	 *          defaultv2
+	 *          v1
+	 *          v2
+	 * @param subsystem the names can be:
+	 * 		    ss1Info
+	 * 			ss2Info
+	 * 			ss3Info
+	 * @return the value interested in
+	 */
+	public String getValue(String key, String subsystem){
+
+		//Obtain the file
+		File f = new File ("docs/"+ subsystem+ ".xml");
+		LinkedHashMap h = XMLManager.getInstance().read(f);
+		String value = h.get(key).toString();
+
+		return value;
+	}
+	
+	/**
+	 * This method is to recover the information in the statistics file.
+	 * @param key The name of the value in the xml file that we want to 
+	 * recover. This can be:
+	 *          procesedPackages
+	 *          faultyPackages
+	 *          totalCakes
+	 * @return the value 
+	 */
+	public String getStatistics(String key){
+
+		File f = new File ("docs/statistics.xml");
+		LinkedHashMap h = XMLManager.getInstance().read(f);
+		String value = h.get(key).toString();
+
+		return value;
+	}
+	
+	/**
+	 * This method is to recover the information of the subsystems.
+	 * @param subsystem the names can be:
+	 * 		    ss1Info
+	 * 			ss2Info
+	 * 			ss3Info
+	 * @param key The name of the value in the xml file that we want to 
+	 * recover. This can be:
+	 *          defaultSpeed
+	 *          conveyorSpeed
+	 *          defaultv1
+	 *          defaultv2
+	 *          v1
+	 *          v2
+	 * @param value
+	 */
+	public void setValues(String subsystem, String key, String value){
+		File f = new File ("docs/"+ subsystem+ ".xml");
+		LinkedHashMap h = XMLManager.getInstance().read(f);
+		//the value is replaced in the hashmap
+		if (h.containsKey(key)==false){
+			System.out.println("Error: there is no key " + key + "in the statistics file");
+		}else{
+			h.put(key, value);
+			XMLManager.getInstance().generate(subsystem, "docs/"+ subsystem+ ".xml", h);
+		}
+	}
+	
+	/**
+	 * This method is to set the information in the statistics file.
+	 * @param key The name of the value in the xml file that we want to 
+	 * recover. This can be:
+	 *          procesedPackages
+	 *          faultyPackages
+	 *          totalCakes
+	 * @param value 
+	 */
+	public void setStatistics(String key, String value){
+		File f = new File ("docs/statistics.xml");
+		LinkedHashMap h = XMLManager.getInstance().read(f);
+		//the value is replaced in the hashmap
+		if (h.containsKey(key)==false){
+			System.out.println("Error: there is no key " + key + "in the statistics file");
+		}else{
+			h.put(key, value);
+			XMLManager.getInstance().generate("statistics", "docs/statistics.xml", h);
+		}
+	}
 
 }
