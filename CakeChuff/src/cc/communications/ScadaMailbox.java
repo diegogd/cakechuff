69f3f75e51626fd0f9f265d6f709e23ea5da30d8
selvaya <selvaya@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-15 19:08:56 +0000

    El buzon de scada

---
+++ CakeChuff/src/cc/communications/ScadaMailbox.java
@@ -0,0 +1,66 @@
+package cc.communications;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import cc.scada.SCADA;
+
+public class ScadaMailbox implements Runnable {
+
+	private ServerSocket ss;
+	private Socket so;
+	private DataInputStream din; 
+	private DataOutputStream dout;
+	private ArrayList<String> msg_list;
+	private SCADA owner;
+	private boolean failure;
+	public boolean isFailure() {
+		return failure;
+	}
+
+	public void setFailure(boolean failure) {
+		this.failure = failure;
+	}
+
+	public ScadaMailbox(SCADA owner, int port) throws IOException{
+		this.owner=owner;
+		ss = new ServerSocket(port);
+		msg_list=new ArrayList<String>();
+	}
+	
+	//@Override
+	public void run() {
+		//Accept connections
+		try{
+			this.connect();
+			while(!failure){
+				receiveMsgs();
+			}
+		}catch(IOException ioe){
+			//cannot connect
+		}
+		
+	}
+
+	private void connect() throws IOException {
+		so = ss.accept();
+		din = new DataInputStream(so.getInputStream());
+		//dout = new DataOutputStream(so.getOutputStream());
+	}
+
+	private void receiveMsgs() {
+		String msg;
+		while (true) {
+			try {
+				msg = din.readUTF();
+				owner.newMsg(msg);				
+			} catch (IOException ioe) {
+				// connection failure
+			}
+		}
+	}
+}
