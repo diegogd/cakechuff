3210c3fc0332899520e37722469a91b47af15293
selvaya <selvaya@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-15 19:08:34 +0000

    Añadida comunicacion entre el master y scada

--- trunk/CakeChuff/src/cc/scada/SCADA.java
+++ trunk/CakeChuff/src/cc/scada/SCADA.java
@@ -1,12 +1,28 @@
 package cc.scada;
+
 import java.io.*;
+import java.net.Socket;
 import java.util.*;
+import cc.communications.ScadaMailbox;
 
 public class SCADA {
 	
+	private ScadaMailbox _mailbox;
+	private Socket sout;
+	private DataOutputStream dout;
+	
+	//The masters keeps listening on port 9000
+	private int portout = 9000;
+	//Scada keeps listening on port 9009
+	private int portscada = 9009;
 
 	public SCADA (){
-
+		try {
+			_mailbox = new ScadaMailbox (this, portscada);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -116,13 +132,13 @@ public class SCADA {
 	 */
 	public String sendAutomatonInfo1(){
 		String send = "";
-		
+
 		send = this.getValue("cakesCapacity", "ss1Info") + "#" +
-                       this.getValue("conveyorSpeed", "ss1Info") + "#" +
-                       this.getValue("conveyorLenght", "ss1Info") + "#" +
-		       this.getValue("v1", "ss1Info")+ "#"+
-		       this.getValue("v2", "ss1Info");
-		
+				this.getValue("conveyorSpeed", "ss1Info") + "#" +
+				this.getValue("conveyorLenght", "ss1Info") + "#" +
+				this.getValue("v1", "ss1Info")+ "#"+
+				this.getValue("v2", "ss1Info");
+
 		return send;
 	}
 	
@@ -132,160 +148,102 @@ public class SCADA {
 	 * @return A string 
 	 */
 	public String sendAutomatonInfo2(){
- 		return this.getValue("conveyorSpeed", "ss2Info") + "#" +
-                        this.getValue("conveyorLenght", "ss2Info");
+		return this.getValue("conveyorSpeed", "ss2Info") + "#" +
+		this.getValue("conveyorLenght", "ss2Info");
 	}
-        
-     private String sendRobotInfo() {
-        return this.getValue("pick_blister", "robot") + "#" +  this.getValue("move_place_cake", "robot");
+
+	private String sendRobotInfo() {
+		return this.getValue("pick_blister", "robot") + "#" +  this.getValue("move_place_cake", "robot");
+	}
+	
+     /**
+      * Method that inform about the the data for automaton 3.
+      * @return A string with the speed
+      */
+     public String sendAutomatonInfo3(){
+    	 String send = "";
+
+    	 send = this.getValue("conveyorSpeed", "ss3Info") + "#" +
+    	 this.getValue("conveyorLenght", "ss3Info") + "#" +
+    	 this.getValue("failure_range", "ss3Info")+ "#"+
+    	 this.getValue("time_pack", "ss3Info")+ "#"+
+    	 this.getValue("pick_blister", "ss3Info");
+
+    	 return send;
      }
 	
 	/**
-	 * Method that inform about the the data for automaton 3.
-	 * @return A string with the speed
-	 */
-	public String sendAutomatonInfo3(){
- 		String send = "";
-		
-		send = this.getValue("conveyorSpeed", "ss3Info") + "#" +
-                       this.getValue("conveyorLenght", "ss3Info") + "#" +
-		       this.getValue("failure_range", "ss3Info")+ "#"+
-                       this.getValue("time_pack", "ss3Info")+ "#"+
-		       this.getValue("pick_blister", "ss3Info");
-		
-		return send;
-	}
-	
-        /**
 	 * Method that send the initial info for cakechuff. Begin with the word
 	 * INIT, follow by ":" and all the data of the subsystems, separed by
-         * "$". The different field for each subsystem are divided by "#".
-         * The order is A1(numcakes#speed#lenght#choc#caramel), A2(speed#lenght),
-         * A3(speed#lenght#failurerate#timepack#robot), R1 (pick#move). 
+	 * "$". The different field for each subsystem are divided by "#".
+	 * The order is A1(numcakes#speed#lenght#choc#caramel), A2(speed#lenght),
+	 * A3(speed#lenght#failurerate#timepack#robot), R1 (pick#move). 
 	 * @return A string 
 	 */
 	public String sendInitInfo(){
 		String s = "INIT" + ":" + this.sendAutomatonInfo1() + "$" + 
-				this.sendAutomatonInfo2() + "$" + 
-				this.sendAutomatonInfo3() + "$" +
-                                this.sendRobotInfo();
+		this.sendAutomatonInfo2() + "$" + 
+		this.sendAutomatonInfo3() + "$" +
+		this.sendRobotInfo();
 		return s;
 	}
   
-        /**
+	/**
 	 * Method that send the info to recover the system. Begin with the word
 	 * RESET, follow by ":" and all the data of the subsystems, separed by
-         * "$". The different field for each subsystem are divided by "#", and the
-         * first one is the state where the subsystem was stopped.
-         * The order is A1(state#numcakes#speed#lenght#choc#caramel), 
-         * A2(state#speed#lenght),A3(state#s_robot#speed#lenght#failurerate#timepack#robot),
-         * R1 (state#pick#move). 
+	 * "$". The different field for each subsystem are divided by "#", and the
+	 * first one is the state where the subsystem was stopped.
+	 * The order is A1(state#numcakes#speed#lenght#choc#caramel), 
+	 * A2(state#speed#lenght),A3(state#s_robot#speed#lenght#failurerate#timepack#robot),
+	 * R1 (state#pick#move). 
 	 * @return A string 
 	 */
-        public String sendResetInfo(){
+	public String sendResetInfo(){
 		String s = "RESET" + ":" + this.getValue("state", "ss1Info") + "#" + this.sendAutomatonInfo1() + "$" + 
-				this.getValue("state", "ss2Info") + "#" + this.sendAutomatonInfo2() + "$" + 
-				this.getValue("state", "ss3Info") + "#" + this.getValue("robot_state", "ss3Info") + "#" +this.sendAutomatonInfo3() + "$" +
-                                this.getValue("state", "robot") + "#" + this.sendRobotInfo();
+		this.getValue("state", "ss2Info") + "#" + this.sendAutomatonInfo2() + "$" + 
+		this.getValue("state", "ss3Info") + "#" + this.getValue("robot_state", "ss3Info") + "#" +this.sendAutomatonInfo3() + "$" +
+		this.getValue("state", "robot") + "#" + this.sendRobotInfo();
 		return s;
 	}
         
         /**
-	 * Method that send the info to restart one subsystem, in response a 
+         * Method that send the info to restart one subsystem, in response a 
          * petitions from the Master automaton. 
-	 * @return A string 
-	 */
+         * @return A string 
+         */
         public String sendRestartInfo(String subsystem_name){
-            String s = "RESTART" + ":" + subsystem_name + "$";
-                
-            if (subsystem_name.compareTo("A1") ==0){
-                s.concat(this.getValue("state", "ss1Info") + "#" + this.sendAutomatonInfo1());
-            }else if(subsystem_name.compareTo("A2") ==0){
-                s.concat(this.getValue("state", "ss2Info") + "#" + this.sendAutomatonInfo2());
-            }else if (subsystem_name.compareTo("A3") ==0){
-                s.concat(this.getValue("state", "ss3Info") + "#" +  
-                        this.getValue("robot_state", "ss3Info") + "#"+ this.sendAutomatonInfo3());
-            }else if(subsystem_name.compareTo("R1") ==0){
-               s.concat(this.getValue("state", "robot") + "#" + this.sendRobotInfo());
-            }
-               
-            return s;
-	}
-     
-        public void receiveRecover(){
-        	String a ="RECOVER:AUTOMATA";
+        	String s = "RESTART" + ":" + subsystem_name + "$";
+
+        	if (subsystem_name.compareTo("A1") ==0){
+        		s.concat(this.getValue("state", "ss1Info") + "#" + this.sendAutomatonInfo1());
+        	}else if(subsystem_name.compareTo("A2") ==0){
+        		s.concat(this.getValue("state", "ss2Info") + "#" + this.sendAutomatonInfo2());
+        	}else if (subsystem_name.compareTo("A3") ==0){
+        		s.concat(this.getValue("state", "ss3Info") + "#" +  
+        				this.getValue("robot_state", "ss3Info") + "#"+ this.sendAutomatonInfo3());
+        	}else if(subsystem_name.compareTo("R1") ==0){
+        		s.concat(this.getValue("state", "robot") + "#" + this.sendRobotInfo());
+        	}
+
+        	return s;
         }
         
-       /**
-	 * Method that send the message to master automaton to stop the system
-	 * @return A string 
-	 */
+        /**
+         * Method that send the message to master automaton to stop the system
+         * @return A string 
+         */
         public String stop(){
-            return "STOP";  
-	}
+        	return "STOP";  
+        }
         
         /**
-	 * Method that send the message to master automaton to stop the system
-         * inmediatetly.
-	 * @return A string 
-	 */
+         * Method that send the message to master automaton to stop the system
+         * immediately.
+         * @return A string 
+         */
         public String emergencyStop(){
-            return "EMERGENCY";  
-	}
-        
-        /**
-	 * Method to reset all the default values of CakeChuff
-	 */
-        public void resetDB(){
-            String defaultval = null;
-            
-            //Automaton 1
-            defaultval = this.getValue("defaultSpeed", "ss1Info");
-            this.setValues("ss1Info", "conveyorSpeed", defaultval);
-            
-            defaultval = this.getValue("defaultLenght","ss1Info");
-            this.setValues("ss1Info", "conveyorLenght", defaultval);
-            
-            defaultval = this.getValue("defaultv1", "ss1Info");
-            this.setValues("ss1Info", "v1", defaultval);
-            
-            defaultval = this.getValue("defaultv2", "ss1Info");
-            this.setValues("ss1Info", "v2", defaultval);
-            
-            defaultval = this.getValue("defaultCapacity", "ss1Info");
-            this.setValues("ss1Info", "cakesCapacity", defaultval);
-
-            //Automaton 2
-            defaultval = this.getValue("defaultSpeed", "ss2Info");
-            this.setValues("ss2Info", "conveyorSpeed", defaultval);
-            
-            defaultval = this.getValue("defaultLenght", "ss2Info");
-            this.setValues("ss2Info", "conveyorLenght", defaultval);
-            
-            //Automaton 3
-            defaultval = this.getValue("defaultSpeed", "ss3Info");
-            this.setValues("ss3Info", "conveyorSpeed", defaultval);
-            
-            defaultval = this.getValue("defaultLenght", "ss3Info");
-            this.setValues("ss3Info", "conveyorLenght", defaultval);
-            
-            defaultval = this.getValue("defaultTime", "ss3Info");
-            this.setValues("ss3Info", "time_pack", defaultval);
-            
-            defaultval = this.getValue("defaultRange", "ss3Info");
-            this.setValues("ss3Info", "failure_range", defaultval);
-            
-            defaultval = this.getValue("defaultTimeRobot", "ss3Info");
-            this.setValues("ss3Info", "pick_blister", defaultval);
-            
-            //Robot
-            defaultval = this.getValue("defaultPick", "robot");
-            this.setValues("robot", "pick_blister", defaultval);
-            
-            defaultval = this.getValue("defaultMove", "robot");
-            this.setValues("robot", "move_place_cake", defaultval);
-            
-	}
+        	return "EMERGENCY";  
+        }
 
         /**
          * Method to receive the message from the master with the info about
@@ -314,6 +272,102 @@ public class SCADA {
             return ok;
         }
 
+        /**
+         * Method invoke by the mailbox when a new message arrives
+         * @param msg The new message
+         */
+		public synchronized void newMsg(String msg) {
+			String [] array = msg.split(":");
+			
+			//The only thing that Scada receives from the master 
+			// is a change in the state, or a petition to restart.
+			if (array[0].compareTo("RESTART") ==0){
+               this.sendtoMaster(sendRestartInfo(array[1]));
+            }else{
+            	if (!this.stateChanged(msg)){
+            		System.out.println("Something wrong changing the state for " + array[1]);
+            	} 	
+            }
+			
+		}
 
+		/**
+		 * Method to send a message to the master
+		 * @param msg The message to be sent
+		 */
+		protected void sendtoMaster(String msg){
+			
+			try{
+				dout.writeChars(msg);
+			}catch(IOException ioe){
+				//restart the receiving connection
+				if(!_mailbox.isFailure())_mailbox.setFailure(true);
+				//connection failure
+				boolean success=false;
+				while(!success){
+					try{
+					sout= new Socket("master", portout);
+					dout = new DataOutputStream(sout.getOutputStream());
+					}catch(IOException ioe2){
+						//keep trying
+					}
+				}
+			}
+			
+	}
+		
+		/**
+         * Method to reset all the default values of CakeChuff
+         */
+        public void resetDB(){
+        	String defaultval = null;
+
+        	//Automaton 1
+        	defaultval = this.getValue("defaultSpeed", "ss1Info");
+        	this.setValues("ss1Info", "conveyorSpeed", defaultval);
+
+        	defaultval = this.getValue("defaultLenght","ss1Info");
+        	this.setValues("ss1Info", "conveyorLenght", defaultval);
+
+        	defaultval = this.getValue("defaultv1", "ss1Info");
+        	this.setValues("ss1Info", "v1", defaultval);
+
+        	defaultval = this.getValue("defaultv2", "ss1Info");
+        	this.setValues("ss1Info", "v2", defaultval);
+
+        	defaultval = this.getValue("defaultCapacity", "ss1Info");
+        	this.setValues("ss1Info", "cakesCapacity", defaultval);
+
+        	//Automaton 2
+        	defaultval = this.getValue("defaultSpeed", "ss2Info");
+        	this.setValues("ss2Info", "conveyorSpeed", defaultval);
+
+        	defaultval = this.getValue("defaultLenght", "ss2Info");
+        	this.setValues("ss2Info", "conveyorLenght", defaultval);
+
+        	//Automaton 3
+        	defaultval = this.getValue("defaultSpeed", "ss3Info");
+        	this.setValues("ss3Info", "conveyorSpeed", defaultval);
+
+        	defaultval = this.getValue("defaultLenght", "ss3Info");
+        	this.setValues("ss3Info", "conveyorLenght", defaultval);
+
+        	defaultval = this.getValue("defaultTime", "ss3Info");
+        	this.setValues("ss3Info", "time_pack", defaultval);
+
+        	defaultval = this.getValue("defaultRange", "ss3Info");
+        	this.setValues("ss3Info", "failure_range", defaultval);
+
+        	defaultval = this.getValue("defaultTimeRobot", "ss3Info");
+        	this.setValues("ss3Info", "pick_blister", defaultval);
+
+        	//Robot
+        	defaultval = this.getValue("defaultPick", "robot");
+        	this.setValues("robot", "pick_blister", defaultval);
+
+        	defaultval = this.getValue("defaultMove", "robot");
+        	this.setValues("robot", "move_place_cake", defaultval);
+
+        }
 	
 }
