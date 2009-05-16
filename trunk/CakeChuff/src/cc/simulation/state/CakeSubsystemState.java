92a0fa2c33967f11a20638a093b09ddcdab3414a
diegogd <diegogd@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-16 20:31:40 +0000

    Añadidos colores a los contenidos líquidos, y añadido chorro.

--- trunk/CakeChuff/src/cc/simulation/state/CakeSubsystemState.java
+++ trunk/CakeChuff/src/cc/simulation/state/CakeSubsystemState.java
@@ -15,10 +15,17 @@ public class CakeSubsystemState extends Observable {
 	private float valve1_open_secs = 0;
 	private float valve2_open_secs = 0;
 	
+	/**
+	 * Constructor of the class, it creates the vector of sensors.
+	 */
 	public CakeSubsystemState() {
 		sensors = new Vector<Sensor>();
 	}
 	
+	/**
+	 * Provides the singleton class
+	 * @return The unique instance of CakeSubsystem
+	 */
 	public static synchronized CakeSubsystemState getInstance()
 	{
 		if( _instance == null ){
@@ -27,33 +34,52 @@ public class CakeSubsystemState extends Observable {
 		return _instance;
 	}
 	
+	/**
+	 * Returns the current conveyor velocity.
+	 * @return Current conveyor velocity.
+	 */
 	public float getConveyor_velocity() {
 		return conveyor_velocity;
 	}
+	
+	/**
+	 * Set a New Velocity to the conveyor.
+	 * @param conveyor_velocity Velocity of the conveyor
+	 */
 	public void setConveyor_velocity(float conveyor_velocity) {
 		this.conveyor_velocity = conveyor_velocity;
 		setChanged();
 		notifyObservers();
 	}
+	
+	/**
+	 * Return the seconds that the conveyor still opened.
+	 * @return
+	 */
 	public float getValve1_open_secs() {
 		return valve1_open_secs;
 	}
+	
 	public void setValve1_open_secs(float valve1_open_secs) {
 		this.valve1_open_secs = valve1_open_secs;
 		setChanged();
 		notifyObservers();
 	}
+	
 	public void resetValve1_open_secs() {
 		this.valve1_open_secs = 0;
 	}
+	
 	public float getValve2_open_secs() {
 		return valve2_open_secs;
 	}
+	
 	public void setValve2_open_secs(float valve2_open_secs) {
 		this.valve2_open_secs = valve2_open_secs;
 		setChanged();
 		notifyObservers();
 	}
+	
 	public void resetValve2_open_secs() {
 		this.valve2_open_secs = 0;
 	}
@@ -72,7 +98,4 @@ public class CakeSubsystemState extends Observable {
 			}
 		}
 	}
-	
-		
-	
 }
