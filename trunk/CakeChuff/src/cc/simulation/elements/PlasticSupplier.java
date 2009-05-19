1e9273c33b9143afac35e13345994653aabecded
diegogd <diegogd@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-19 22:55:55 +0000

    Modificador el color y tamaño del PlasticSupplier.

--- trunk/CakeChuff/src/cc/simulation/elements/PlasticSupplier.java
+++ trunk/CakeChuff/src/cc/simulation/elements/PlasticSupplier.java
@@ -22,17 +22,17 @@ public class PlasticSupplier extends Node{
 	
 	public void initElement(){
 		
-		roll = new Cylinder("pole", 4, 20, 0.2f, 5f,true);
+		roll = new Cylinder("pole", 4, 20, 0.2f, 4.5f,true);
 		roll.setDefaultColor(ColorRGBA.black);
 		roll.setLocalTranslation(0, 0, -0.2f);
 		this.attachChild(roll);
 		
-		roll = new Cylinder("roll", 4, 20, 1f, 4f,true);
-		roll.setDefaultColor(ColorRGBA.blue);
+		roll = new Cylinder("roll", 4, 20, 1f, 3.4f,true);
+		roll.setDefaultColor(ColorRGBA.lightGray);
 		this.attachChild(roll);
 		
-		plastic = new Box("plastic", new Vector3f(0,0.99f,-2f), new Vector3f(2f, 1f, 2f));
-		plastic.setDefaultColor(ColorRGBA.blue);
+		plastic = new Box("plastic", new Vector3f(0,0.99f,-1.7f), new Vector3f(2f, 1f, 1.7f));
+		plastic.setDefaultColor(ColorRGBA.lightGray);
 		plastic.setModelBound(new BoundingBox());
 		plastic.updateModelBound();
 		this.attachChild(plastic);
@@ -42,7 +42,7 @@ public class PlasticSupplier extends Node{
 		plastic.setLocalScale(new Vector3f(plastic.getLocalScale().x+amount/2,1,1));
 		
 		if(plastic.getLocalScale().x > 3)
-			plastic.setLocalScale(new Vector3f(2f,1,1));
+			plastic.setLocalScale(new Vector3f(1.2f,1,1));
 	}
 	
 }
