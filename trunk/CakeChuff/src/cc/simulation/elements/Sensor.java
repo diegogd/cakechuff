2d5c6afca925b72d11e0c8ef562e0437f49af9ac
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 20:49:05 +0000

    Documentation

--- trunk/CakeChuff/src/cc/simulation/elements/Sensor.java
+++ trunk/CakeChuff/src/cc/simulation/elements/Sensor.java
@@ -3,15 +3,27 @@ package cc.simulation.elements;
 import java.util.Observable;
 
 import com.jme.scene.Node;
-
+/**
+ * Implementation and definition of the sensors, one of
+ * the simulation elements that compose CakeChuff system
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class Sensor extends Node {
 
 	private boolean isActived = false;
 	private boolean modified = false;
-	
+	/**
+	 * Returns if the sensor has been activated
+	 * @return True if the robot has been activated, false it has not.
+	 */
 	public boolean isActived() {
 		return isActived;
 	}
+	/**
+	 * Modifies the state of activity of the robot
+	 * @param isActived True: it is active, false: not active
+	 */
 	public void setActived(boolean isActived) {
 		if( this.isActived == isActived )
 			modified = false;
@@ -20,9 +32,17 @@ public class Sensor extends Node {
 			this.isActived = isActived;
 		}
 	}
+	/**
+	 * Returns if the sensor has been modified
+	 * @return True if the robot has been modified, false it has not.
+	 */
 	public boolean isModified() {
 		return modified;
 	}
+	/**
+	 * Modifies the state of modification of the robot
+	 * @param isActived True: it is modified, false: not modified
+	 */
 	public void setModified(boolean modified) {
 		this.modified = modified;
 	}
