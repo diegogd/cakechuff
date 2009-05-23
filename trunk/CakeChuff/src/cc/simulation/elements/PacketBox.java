3788c28f596c3c6aaaf49c3b87519cad824f7507
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 17:59:25 +0000

    Documentation

--- trunk/CakeChuff/src/cc/simulation/elements/PacketBox.java
+++ trunk/CakeChuff/src/cc/simulation/elements/PacketBox.java
@@ -12,7 +12,12 @@ import com.jme.renderer.ColorRGBA;
 import com.jme.scene.Node;
 import com.jme.scene.Spatial;
 import com.jme.scene.shape.Box;
-
+/**
+ * Implementation and definition of the packet box, one of
+ * the simulation elements that compose CakeChuff system
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class PacketBox extends Node{
 	
 	private Node pivot,packetsNode;
@@ -30,7 +35,10 @@ public class PacketBox extends Node{
 		loadModel();
 		this.setName(name);
 	}
-	
+	/**
+	 * Constructor
+	 * Initializes the box of the packet and .
+	 */
 	private void loadBox(){
 		Box table = new Box("PacketBox",new Vector3f(-1,-1,-1),new Vector3f(1,1,1));
 		table.setModelBound(new BoundingBox());
@@ -42,7 +50,9 @@ public class PacketBox extends Node{
 		table.updateRenderState();
 		pivot.attachChild(table);
 	}
-	
+	/**
+	 * Loads the graphical model of the box of the packet
+	 */
 	private void loadModel(){
 		URL model=getClass().getClassLoader().getResource("model/box.obj");
 		pivot.attachChild(ModelLoader.loadOBJ(model, false));
@@ -52,7 +62,10 @@ public class PacketBox extends Node{
 //		aux.setLocalTranslation(0,0.5f,0);
 //		pivot.attachChild(aux);
 	}
-	
+	/**
+	 * Inserts an element to the box of the packets
+	 * @param element The element to be inserted to the box
+	 */
 	public void addInBox(Spatial element){
 		packets.add(element);
 		element.removeFromParent();
@@ -67,13 +80,18 @@ public class PacketBox extends Node{
 		}
 			
 	}
-	
+	/**
+	 * Clears the box of all its elements
+	 */
 	public void emptyBox(){
 		packets.clear();
 		packetsNode.detachAllChildren();
 //		this.setLocalTranslation(f, 0f, 0f);
 	}
-	
+	/**
+	 * Returns the number of packets inside the box
+	 * @return The number of packets inside the box
+	 */
 	public int numOfPackets(){
 		return packets.size();
 	}
