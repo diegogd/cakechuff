2fd581ccb0a932b0f2b81194de53844caa827f2c
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 16:28:46 +0000

    Documentation

--- CakeChuff/src/cc/simulation/elements/ConveyorQuality.java
+++ CakeChuff/src/cc/simulation/elements/ConveyorQuality.java
@@ -5,15 +5,28 @@ import java.net.URL;
 import cc.simulation.utils.ModelLoader;
 
 import com.jme.math.Vector3f;
-
+/**
+ * Implementation and definition of the conveyor quality, one of
+ * the simulation elements that compose CakeChuff system.
+ * Extends its functionality from the "normal" conveyor.
+ * It specifies the conveyor of the quality part of the system
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class ConveyorQuality extends Conveyor {
 
 	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Constructor
+	 * Loads the graphical model of the conveyor
+	 */
 	public ConveyorQuality() {
 //		super("ConveyorCake");
 		loadModel();
 	}
+	/**
+	 * Loads the graphical model of the conveyor
+	 */
 	public void loadModel(){
 		URL model = getClass().getClassLoader().getResource("model/blisterconveyor.obj");						
 		
@@ -24,7 +37,10 @@ public class ConveyorQuality extends Conveyor {
 //		plc.setLocalTranslation(3, 4, 2);
 //		this.attachChild(plc);
 	}	
-	
+	/**
+	 * Modifies the current velocity of the conveyor
+	 * @param velocity The new velocity of the conveyor
+	 */
 	@Override
 	public void setVelocity(float velocity) {
 		super.current_velocity = velocity;
