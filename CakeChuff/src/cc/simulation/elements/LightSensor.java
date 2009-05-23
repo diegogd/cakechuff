41716c2b707740ec101f6ed2d510c02c7c49d1ce
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 17:37:11 +0000

    Documentation

--- CakeChuff/src/cc/simulation/elements/LightSensor.java
+++ CakeChuff/src/cc/simulation/elements/LightSensor.java
@@ -4,11 +4,20 @@ import com.jme.bounding.BoundingBox;
 import com.jme.renderer.ColorRGBA;
 import com.jme.scene.Node;
 import com.jme.scene.shape.Cylinder;
-
+/**
+ * Implementation and definition of the light sensor, one of
+ * the simulation elements that compose CakeChuff system
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class LightSensor extends Sensor {
 	
 	Cylinder light;
-
+	/**
+	 * Constructor
+	 * Initializes the light sensor (color of the light and graphical model)
+	 * @param id Identification of the light sensor
+	 */
 	public LightSensor(String id) {
 		setName(id);
 		light = new Cylinder(id, 20, 20, 0.05f, 2, true);
@@ -18,13 +27,17 @@ public class LightSensor extends Sensor {
 		light.updateRenderState();
 		this.attachChild(light);
 	}
-	
+	/**
+	 * Sets the light sensor on (color: green)
+	 */
 	public void setOn()
 	{
 		light.setDefaultColor(ColorRGBA.green);
 		this.setActived(true);
 	}
-	
+	/**
+	 * Sets the light sensor off (color: red)
+	 */
 	public void setOff()
 	{
 		light.setDefaultColor(ColorRGBA.red);
