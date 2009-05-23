3ae4545498a37b9c343328b9bd220e1139ab5c9d
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 16:14:43 +0000

    Documentation

--- trunk/CakeChuff/src/cc/simulation/elements/ConveyorCake.java
+++ trunk/CakeChuff/src/cc/simulation/elements/ConveyorCake.java
@@ -17,16 +17,27 @@ import com.jme.scene.Spatial;
 import com.jme.util.export.binary.BinaryImporter;
 import com.jmex.model.converters.FormatConverter;
 import com.jmex.model.converters.ObjToJme;
-
+/**
+ * Implementation and definition of the conveyor cake, one of
+ * the simulation elements that compose CakeChuff system.
+ * Extends its functionality from the "normal" conveyor.
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class ConveyorCake extends Conveyor {
 
 	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Constructor
+	 * Loads the graphical model of the conveyor
+	 */
 	public ConveyorCake() {
 		// super("CakeSystem");
 		loadModel();
 	}
-	
+	/**
+	 * Loads the graphical model of the conveyor
+	 */
 	public void loadModel(){
 		URL model = getClass().getClassLoader().getResource("model/cakeconveyor.obj");						
 		
