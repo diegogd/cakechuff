1c08d936d2ac099c7868c1587473fd22fc1eb5aa
guillermocabanas <guillermocabanas@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 16:40:15 +0000

    QCAutomaton.java JavaDoc comments

--- CakeChuff/src/cc/automatons/QCAutomaton.java
+++ CakeChuff/src/cc/automatons/QCAutomaton.java
@@ -13,6 +13,11 @@ import cc.simulation.elements.TouchSensor;
 import cc.simulation.state.QualitySubsystemState;
 
 
+/**
+ * Define the internal logic of the QCAutomaton
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class QCAutomaton extends Automaton {
 	
 
@@ -35,6 +40,17 @@ public class QCAutomaton extends Automaton {
 	//simulation
 	QualitySubsystemState qcsystem;
 	boolean passed;
+	
+	/**
+	 * Constructor
+	 * Initialize the communication channel (mailbox) with the MasterAutomaton 
+	 * @param portin Input port of the Mailbox with the MasterAutomaton
+	 * @param portout Output port of the Mailbox with the MasterAutomaton
+	 * @param master Destination address of the Mailbox with the MasterAutomaton
+	 * @exception UnknownHostException Communication error
+	 * @exception IOException Communication error
+	 * @exception SecurityException Security error
+	 */
 	public QCAutomaton(int portin, int portout, String master){
 		state=START;
 		try{
@@ -64,6 +80,15 @@ public class QCAutomaton extends Automaton {
 			
 		}
 	}
+	
+	/**
+	 * Set the QCAutomaton start parameters
+	 * @param speed	Conveyor belt speed
+	 * @param belt_lg Conveyor belt length
+	 * @param f_rate Failure rate
+	 * @param t_stamp Stamper time
+	 * @param t_rob Robot time
+	 */
 	private void run_start(int speed,int belt_lg,int f_rate,int t_stamp,int t_rob){
 		this.speed = (float)speed/(belt_lg*3);
 		this.belt_lg=belt_lg;
@@ -75,6 +100,12 @@ public class QCAutomaton extends Automaton {
 		run_init();
 		
 	}
+	
+	/**
+	 * The conveyor belt has to move
+	 * Quality&Control Automaton state: INIT
+	 * Set the conveyor belt speed
+	 */
 	private void run_init(){
 		if(!stop){
 			qcsystem.setConveyor_velocity(speed);
@@ -82,6 +113,13 @@ public class QCAutomaton extends Automaton {
 			send("A3:init");
 		}
 	}
+	
+	/**
+	 * The sensor have to test the quality
+	 * Quality&Control Automaton state: QC
+	 * Stop the conveyor belt
+	 * Depending of the quality test result, blisters are classified
+	 */
 	private void run_QC(){
 		qcsystem.setConveyor_velocity(0);
 		state=QC;
@@ -105,11 +143,22 @@ public class QCAutomaton extends Automaton {
 			run_ko_mov();
 		}
 	}
+	
+	/**
+	 * Quality&Control Automaton state: QC_STAMP
+	 * Restart the conveyor belt
+	 */
 	private void run_qc_stamp(){
 		qcsystem.setConveyor_velocity(speed);
 		state=QC_STAMP;
 		send("A3:qc_stamp");		
 	}
+	
+	/**
+	 * The blister has to be stamped
+	 * Stop the conveyor belt
+	 * Stamp the blister
+	 */
 	private void run_stamp(){
 		qcsystem.setConveyor_velocity(0);
 		qcsystem.setWrapper_secs(t_stamp);
@@ -121,17 +170,34 @@ public class QCAutomaton extends Automaton {
 		}
 		run_stamp_wait();
 	}
+	
+	/**
+	 * Quality&Control Automaton state: STAMP_WAIT
+	 * Restart the conveyor belt
+	 */
 	private void run_stamp_wait(){
 		qcsystem.setConveyor_velocity(speed);
 		state=STAMP_WAIT;
 		send("A3:stamp_wait");
-		
 	}
+	
+	/**
+	 * The blister has not passed the quality control
+	 * Quality&Control Automaton state: KO_MOV
+	 * Restart the conveyor belt
+	 */
 	private void run_ko_mov(){
 		qcsystem.setConveyor_velocity(speed);
 		state=KO_MOV;
 		send("A3:ko_mov:"+cakes_blister);
 	}
+	
+	/**
+	 * The blister has passed the quality control and it has to wait
+	 * Quality&Control Automaton state: KO_WAIT
+	 * Stop the conveyor belt
+	 * Robot transition state to PICKUPPACKET
+	 */
 	private void run_ok_wait(){
 		qcsystem.setConveyor_velocity(0);
 		state=OK_WAIT;
@@ -139,6 +205,13 @@ public class QCAutomaton extends Automaton {
 		//pick and box
 		qcsystem.setRobotGoToState(qcsystem.PICKUPPACKET);
 	}
+	
+	/**
+	 * The blister has not passed the quality control and it has to wait
+	 * Quality&Control Automaton state: KO_WAIT
+	 * Stop the conveyor belt
+	 * Robot transition state to PICKUPPACKET
+	 */
 	private void run_ko_wait(){
 		qcsystem.setConveyor_velocity(0);
 		state=KO_WAIT;
@@ -146,6 +219,11 @@ public class QCAutomaton extends Automaton {
 		//pick and dispose
 		qcsystem.setRobotGoToState(qcsystem.PICKUPPACKET);
 	}
+	
+	
+	/**
+	 * Recover from a failure
+	 */
 	private void run_failure(String data){
 		//TODO: Ver que hacer con el estado del robot 
 		String pars[]=data.split("#");
@@ -178,12 +256,21 @@ public class QCAutomaton extends Automaton {
 			(new Thread(this)).start();
 		}
 	}
+	
+	/**
+	 * Restart after a stop
+	 */
 	private void run_stop(){
 		qcsystem.setConveyor_velocity(0);
 		qcsystem.deleteObserver(this);
 		qcsystem.setRobotMoving(false);
 		state=START;
 	}
+	
+	/**
+	 * Manage the control and synchronize messages received from the MasterAutomaton
+	 * @param msg Message received from the MasterAutomaton
+	 */
 	@Override
 	public synchronized void newMsg(String msg) {
 		String[] content= msg.split(":");
@@ -221,6 +308,10 @@ public class QCAutomaton extends Automaton {
 		
 		}
 	}
+	
+	/**
+	 * Run the QualityAutomaton
+	 */
 	public void run() {
 		switch (state) {
 		case QC:
@@ -237,6 +328,11 @@ public class QCAutomaton extends Automaton {
 			break;
 		}
 	}
+	
+	/**
+	 * Produce state transition depending on the signals received from the sensors
+	 * @param arg Sensor in the conveyor belt which detects the position and quality of the blister
+	 */
 	@Override
 	public void update(Observable o, Object arg) {
 		// which sensor?
@@ -287,7 +383,11 @@ public class QCAutomaton extends Automaton {
 		}
 
 	}
-	//stop because we want an "unclean", sudden stop
+	
+	/**
+	 * Stop the QCAutomaton's communication channel (mailbox)
+	 * Deprecated stop method is employed for an "unclean" sudden stop
+	 */
 	@SuppressWarnings("deprecation")
 	public void destroyAutomaton(){
 		qcsystem.setConveyor_velocity(0);
@@ -295,6 +395,11 @@ public class QCAutomaton extends Automaton {
 		qcsystem.setQualityCheck(false);
 		mbox_thread.stop();
 	}
+	
+	/**
+	 * Execute the QCAutomaton independently
+	 * @param args The command line arguments
+	 */
 	public static void main(String args[]){
 		QCAutomaton aut=new QCAutomaton(Integer.parseInt(args[0]),Integer.parseInt(args[1]),args[2]);
 		while(true){
