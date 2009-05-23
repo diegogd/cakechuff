ae2aa93df9dd88ba9e2e34682d5edc122c9df71c
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 14:41:45 +0000

    Documentation

--- trunk/CakeChuff/src/cc/sensors/Sensor.java
+++ trunk/CakeChuff/src/cc/sensors/Sensor.java
@@ -1,9 +1,20 @@
 package cc.sensors;
 
+import java.io.IOException;
+
+/**
+ * Definition of the implementation of the sensors of the CakeChuff system
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public abstract class Sensor {
 
 	private String idSensor;
-	
+	/**
+	 * Constructor
+	 * Initializes the identification of the sensor
+	 * @param id Sensor identification
+	 */
 	public Sensor(String id) {
 		this.idSensor = id;
 	}
