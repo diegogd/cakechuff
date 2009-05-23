b71097f1640730ef8c24650eb67ec91c5ab028e8
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 16:12:12 +0000

    Documentation

--- CakeChuff/src/cc/simulation/elements/ConveyorBlister.java
+++ CakeChuff/src/cc/simulation/elements/ConveyorBlister.java
@@ -5,17 +5,28 @@ import java.net.URL;
 import cc.simulation.utils.ModelLoader;
 
 import com.jme.math.Vector3f;
-
+/**
+ * Implementation and definition of the conveyor blister, one of
+ * the simulation elements that compose CakeChuff system.
+ * Extends its functionality from the "normal" blister.
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class ConveyorBlister extends Conveyor {
 
 	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Constructor
+	 * Loads the graphical model of the conveyor
+	 */
 	public ConveyorBlister() {
 //		 super("CakeSystem");
 		loadModel();
 	}
 	
-	
+	/**
+	 * Loads the graphical model of the conveyor
+	 */
 	public void loadModel(){
 		URL model = getClass().getClassLoader().getResource("model/blisterconveyor.obj");						
 		
@@ -26,7 +37,10 @@ public class ConveyorBlister extends Conveyor {
 //		plc.setLocalTranslation(3, 4, 2);
 //		this.attachChild(plc);
 	}	
-
+	/**
+	 * Modifies the current velocity of the conveyor
+	 * @param velocity The new velocity of the conveyor
+	 */
 	@Override
 	public void setVelocity(float velocity) {
 		super.current_velocity = velocity;
