cbfc9ca6fd47ad9f54605b7fb4358c1a78def494
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 16:46:15 +0000

    Documentation

--- trunk/CakeChuff/src/cc/simulation/elements/Cutter.java
+++ trunk/CakeChuff/src/cc/simulation/elements/Cutter.java
@@ -10,7 +10,12 @@ import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
 import com.jme.scene.Node;
 import com.jme.scene.shape.Box;
-
+/**
+ * Implementation and definition of the cutter, one of
+ * the simulation elements that compose CakeChuff system
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class Cutter extends Node {
 
 	
@@ -21,7 +26,10 @@ public class Cutter extends Node {
 	Node pivot;
 	
 	private boolean direction;
-
+	/**
+	 * Constructor
+	 * Initializes cutters direction and speed. Also loads it in the graphic interface.
+	 */
 	public Cutter() {
 //		loadModel();
 
@@ -31,14 +39,23 @@ public class Cutter extends Node {
 		direction = false;
 		speed = 0;
 	}
-	
+	/**
+	 * Returns the speed of the cutterr
+	 * @return The speed of the cutter
+	 */
 	public float getSpeed(){
 		return this.speed;
 	}
+	/**
+	 * Modifies the speed of the cutter
+	 * @param speed The new speed of the cutter
+	 */
 	public void setSpeed(float speed){
 		this.speed = speed;
 	}
-	
+	/**
+	 * Cutter is graphically displayed as a box
+	 */
 	private void loadBox(){
 		
 		Box cutter = new Box("cutter",new Vector3f(-2f,0.99f,-2f), new Vector3f(2f, 1f, 2f));
@@ -51,7 +68,11 @@ public class Cutter extends Node {
 		cutter.getLocalRotation().fromAngles(angles);
 		pivot.attachChild(cutter);
 	}
-	
+	/**
+	 * Updates the position, direction and speed of the cutter depending on the time
+	 * per frames
+	 * @param timeperframes Depending of this parameter the translation of the cutter will be different
+	 */
 	public void update(float timeperframes)
 	{
 //		System.out.println(pivot.getLocalTranslation().y);
