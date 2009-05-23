19377883fd119a39a08ecdf63c52e07139ced2ae
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-23 14:45:43 +0000

    Documentation

--- trunk/CakeChuff/src/cc/sensors/LightSensor.java
+++ trunk/CakeChuff/src/cc/sensors/LightSensor.java
@@ -1,5 +1,9 @@
 package cc.sensors;
-
+/**
+ * Implementation of the light sensors of the CakeChuff system
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class LightSensor extends Sensor {
 
 	public LightSensor(String id) {
