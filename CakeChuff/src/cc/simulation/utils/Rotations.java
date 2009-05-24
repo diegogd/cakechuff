bb11f6ed400d5f7ec09959b6ef2a91b8dbb9c80f
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-24 12:31:29 +0000

    Documentation

--- CakeChuff/src/cc/simulation/utils/Rotations.java
+++ CakeChuff/src/cc/simulation/utils/Rotations.java
@@ -3,21 +3,37 @@ package cc.simulation.utils;
 import com.jme.math.FastMath;
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
-
+/**
+ * Utility class used in the simulation. Helps in the rotation of objects
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class Rotations {
-
+	/**
+	 * Makes a rotation in the x axis a specific number of radians
+	 * @param radians Number of radians to be rotated
+	 * @return Quaternion after the rotation has been done
+	 */
 	public static Quaternion rotateX(float radians){
 		Quaternion q = new Quaternion();
 		q.fromAngleAxis(FastMath.PI*radians, Vector3f.UNIT_X);
 		return q;
 	}
-	
+	/**
+	 * Makes a rotation in the y axis a specific number of radians
+	 * @param radians Number of radians to be rotated
+	 * @return Quaternion after the rotation has been done
+	 */
 	public static Quaternion rotateY(float radians){
 		Quaternion q = new Quaternion();
 		q.fromAngleAxis(FastMath.PI*radians, Vector3f.UNIT_Y);
 		return q;
 	}
-	
+	/**
+	 * Makes a rotation in the z axis a specific number of radians
+	 * @param radians Number of radians to be rotated
+	 * @return Quaternion after the rotation has been done
+	 */
 	public static Quaternion rotateZ(float radians){
 		Quaternion q = new Quaternion();
 		q.fromAngleAxis(FastMath.PI*radians, Vector3f.UNIT_Z);
