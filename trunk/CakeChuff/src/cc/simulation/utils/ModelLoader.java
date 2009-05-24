60b475205b61a218489f3df70826bbe3ac473cd3
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-24 12:27:35 +0000

    Documentation

--- trunk/CakeChuff/src/cc/simulation/utils/ModelLoader.java
+++ trunk/CakeChuff/src/cc/simulation/utils/ModelLoader.java
@@ -10,9 +10,18 @@ import com.jme.scene.Spatial;
 import com.jme.util.export.binary.BinaryImporter;
 import com.jmex.model.converters.FormatConverter;
 import com.jmex.model.converters.ObjToJme;
-
+/**
+ * Utility class used in the simulation. Loads graphical objects (models)
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class ModelLoader {
-
+	/**
+	 * Loads a specific graphical object or model
+	 * @param pathmodel Path where the model is
+	 * @param boundingBox True if a bounding box is needed, false if it is not.
+	 * @return The model that wants to be loaded
+	 */
 	public static Spatial loadOBJ(URL pathmodel, boolean boundingBox)
 	{
 		FormatConverter converter = new ObjToJme();
