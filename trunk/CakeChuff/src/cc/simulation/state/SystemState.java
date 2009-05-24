a37ad9c534510aeb0e7edd26cff497b86b4024c0
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-24 16:15:06 +0000

    Documentation

--- trunk/CakeChuff/src/cc/simulation/state/SystemState.java
+++ trunk/CakeChuff/src/cc/simulation/state/SystemState.java
@@ -4,7 +4,11 @@ import java.util.Observable;
 import java.util.Vector;
 
 import cc.simulation.elements.Sensor;
-
+/**
+ * Implementation and definition of the state of the system (control of cameras, blisters, cakes etc.)
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class SystemState extends Observable {
 	
 	private static SystemState _instance = null;
@@ -13,7 +17,10 @@ public class SystemState extends Observable {
 	private boolean dropCake = false;
 	private boolean makeBlister = false;
 	private boolean makePacket = false;
-	
+	/**
+	 * Provides the singleton class
+	 * @return The unique instance of the system state
+	 */
 	public static synchronized SystemState getInstance()
 	{
 		if( _instance == null ){
@@ -21,27 +28,40 @@ public class SystemState extends Observable {
 		}
 		return _instance;
 	}
-
+	/**
+	 * Returns the identification of the camera
+	 * @return The identification of the camera
+	 */
 	public int getId_camera() {
 		return id_camera;
 	}
-	
+	/**
+	 * Sets that no camera is being used
+	 */
 	public void resetCamera(){
 		id_camera = -1;
 	}
-
+	/**
+	 * Sets the camera to be used
+	 * @param id_camera Identification of the camera
+	 */
 	public void setId_camera(int id_camera) {
 		this.id_camera = id_camera;
 		setChanged();
 		notifyObservers();
 	}			
-	
+	/**
+	 * Sets that a cake is dropped and notifies the observers
+	 */
 	public void setDropCake() {
 		this.dropCake = true;
 		setChanged();
 		notifyObservers();
 	}
-	
+	/**
+	 * Returns if a cake has been dropped.
+	 * @return True if a cake has been dropped, false if not.
+	 */
 	public boolean dropCake() {
 		if(dropCake){
 			dropCake = false;
@@ -50,13 +70,18 @@ public class SystemState extends Observable {
 		else
 			return dropCake;			
 	}
-	
+	/**
+	 * Sets that the blister has been made
+	 */
 	public void setMakeBlister() {
 		this.makeBlister = true;
 		setChanged();
 		notifyObservers();
 	}
-	
+	/**
+	 * Returns if a blister has been made.
+	 * @return True if a blister has been made., false if not.
+	 */
 	public boolean makeBlister() {
 		if(makeBlister){
 			makeBlister = false;
