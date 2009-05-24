bf52d22505a49d04058ea1ab76f96470c0b054ef
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-24 14:27:52 +0000

    Documentation

--- trunk/CakeChuff/src/cc/simulation/state/CakeSubsystemState.java
+++ trunk/CakeChuff/src/cc/simulation/state/CakeSubsystemState.java
@@ -53,41 +53,59 @@ public class CakeSubsystemState extends Observable {
 	}
 	
 	/**
-	 * Return the seconds that the conveyor still opened.
-	 * @return valve1_open_secs
+	 * Return the seconds that the conveyor is still opened.
+	 * @return The seconds that the conveyor is still opened.
 	 */
 	public float getValve1_open_secs() {
 		return valve1_open_secs;
 	}
-	
+	/**
+	 * Modifies the seconds that the valve 1 is still opened.
+	 * @param The new value for the seconds that the valve 1 is still opened.
+	 */
 	public void setValve1_open_secs(float valve1_open_secs) {
 		this.valve1_open_secs = valve1_open_secs;
 		setChanged();
 		notifyObservers();
 	}
-	
+	/**
+	 * Modifies the seconds that the valve 1 is still opened to zero.
+	 */
 	public void resetValve1_open_secs() {
 		this.valve1_open_secs = 0;
 	}
-	
+	/**
+	 * Return the seconds that the valve 2 is still opened.
+	 * @return The seconds that the valve 2 is still opened.
+	 */
 	public float getValve2_open_secs() {
 		return valve2_open_secs;
 	}
-	
+	/**
+	 * Modifies the seconds that the valve 2 is still opened.
+	 * @param The new value for the seconds that the valve 2 is still opened.
+	 */
 	public void setValve2_open_secs(float valve2_open_secs) {
 		this.valve2_open_secs = valve2_open_secs;
 		setChanged();
 		notifyObservers();
 	}
-	
+	/**
+	 * Modifies the seconds that the valve 1 is still opened to zero.
+	 */
 	public void resetValve2_open_secs() {
 		this.valve2_open_secs = 0;
 	}
-	
+	/**
+	 * Inserts a new sensor in the cake subsystem
+	 * @param s Sensor to be inserted
+	 */
 	public void addSensor(Sensor s){
 		sensors.add(s);
 	}
-	
+	/**
+	 * Checks for any changes in the sensors the subsystem has. In case a change is detected observers are notified.
+	 */
 	public void checkSensorsChanges(){
 		for(int i=0; i< sensors.size(); i++)
 		{
