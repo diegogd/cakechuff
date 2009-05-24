d06f2e94013ce36cba1cf4ae99b60895dbca17e2
pabloae <pabloae@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-24 12:23:10 +0000

    Documentation

--- trunk/CakeChuff/src/cc/simulation/subsystems/Robot1.java
+++ trunk/CakeChuff/src/cc/simulation/subsystems/Robot1.java
@@ -11,7 +11,11 @@ import com.jme.math.FastMath;
 import com.jme.math.Vector3f;
 import com.jme.scene.Node;
 import com.jme.scene.Spatial;
-
+/**
+ * Implementation of the robots used in the simulation 
+ * @version 1.0, 29/05/09
+ * @author CaKeChuff team
+ */
 public class Robot1 extends Node implements Observer {
 	
 	private static final long serialVersionUID = 6232189439190248042L;
@@ -38,7 +42,10 @@ public class Robot1 extends Node implements Observer {
 	// final int DROPBLISTER = 8;
 	final int PICKUPPACKET = 9;
 	final int DROPINSUB3 = 10;
-
+	/**
+	 * Constructor
+	 * Initializes the attributes of the robot and its state
+	 */
 	public Robot1() {
 		// super(new Vector3f(0,0,0));
 
@@ -49,37 +56,62 @@ public class Robot1 extends Node implements Observer {
 
 		phase = 0;
 	}
-
+	/**
+	 * Makes the robot move to the initial position
+	 * @param time Time it takes the robot to do the movement
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean moveToInit(float time) {
 		if (robot.moveTo(0, time))
 			return true;
 		return false;
 	}
-
+	/**
+	 * Makes the robot move to a the position where the subsystem 1 is.
+	 * @param time Time it takes the robot to do the movement
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean moveToSub1(float time) {
 		if (robot.moveTo(angleSubsystem1, time))
 			return true;
 		return false;
 	}
-
+	/**
+	 * Makes the robot move to a the position where the subsystem 2 is.
+	 * @param time Time it takes the robot to do the movement
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean moveToSub2(float time) {
 		if (robot.moveTo(angleSubsystem2, time))
 			return true;
 		return false;
 	}
-
+	/**
+	 * Makes the robot move to a the position where the subsystem 3 is.
+	 * @param time Time it takes the robot to do the movement
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean moveToSub3(float time) {
 		if (robot.moveTo(angleSubsystem3, time))
 			return true;
 		return false;
 	}
-
+	/**
+	 * Makes the robot move to a the position where the table is.
+	 * @param time Time it takes the robot to do the movement
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean moveToTable(float time) {
 		if (robot.moveTo(angleTable, time))
 			return true;
 		return false;
 	}
-
+	/**
+	 * Makes the robot pick up a cake
+	 * @param time Time it takes the robot to do the movement
+	 * @param elements List of cakes to be picked
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean pickUpCake(float time, List<Spatial> elements) {
 		
 		if (robot.getHasObject()) {
@@ -125,7 +157,12 @@ public class Robot1 extends Node implements Observer {
 		return false;
 
 	}
-
+	/**
+	 * Makes the robot drop a cake
+	 * @param time Time it takes the robot to do the movement
+	 * @param element Cake to be dropped
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean dropCake(float time, Spatial element) {
 
 		if (!robot.getHasObject()) {
@@ -162,7 +199,12 @@ public class Robot1 extends Node implements Observer {
 		}
 		return false;
 	}
-
+	/**
+	 * Makes the robot pick up a blister
+	 * @param time Time it takes the robot to do the movement
+	 * @param elements List of blisters to be picked
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean pickUpBlister(float time, List<Spatial> elements) {
 
 		if (robot.getHasObject()) {
@@ -203,7 +245,12 @@ public class Robot1 extends Node implements Observer {
 		}
 		return false;
 	}
-
+	/**
+	 * Makes the robot drop a blister
+	 * @param time Time it takes the robot to do the movement
+	 * @param element Blister to be dropped
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean dropBlister(float time, Spatial target) {
 		
 		if (!robot.getHasObject()) {
@@ -240,7 +287,12 @@ public class Robot1 extends Node implements Observer {
 		}
 		return false;
 	}
-
+	/**
+	 * Makes the robot pick up a packet
+	 * @param time Time it takes the robot to do the movement
+	 * @param elements List of packets to be picked
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean pickUpPacket(float time, List<Spatial> elements) {
 		
 		if (robot.getHasObject()) {
@@ -281,7 +333,12 @@ public class Robot1 extends Node implements Observer {
 		}
 		return false;
 	}
-
+	/**
+	 * Makes the robot drop a packet
+	 * @param time Time it takes the robot to do the movement
+	 * @param target Where to drop the packet
+	 * @return True -> Movement done with no problems; False -> Could not make the movement
+	 */
 	public boolean dropPacket(float time, Spatial target) {
 		
 		if (!robot.getHasObject()) {
@@ -318,7 +375,11 @@ public class Robot1 extends Node implements Observer {
 		}
 		return false;
 	}
-
+	/**
+	 * Updates the state of the valves (opens them x seconds) of the cake subsystem 
+	 * @param arg0 Observable object
+	 * @param arg1 Object
+	 */
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		if (_state.getRobot_velocity() > 0) {
@@ -326,8 +387,11 @@ public class Robot1 extends Node implements Observer {
 		}
 	}
 
+
 	/**
-	 * Gestionar los estados y las llamadas a las funciones en el update
+	 * Updates the state of the robot depending on the actual state 
+	 * @param time Time for the robot to make its movement / change its state.
+	 * @param factory Composition of all the elements of the simulation of the system
 	 */
 	public void update(float time, Factory factory) {
 		if (_state.getIfMoving()) {
