1aac98aabff7cf1c85d311f4f37e9d071018b3b8
dhivesh.sadhwani <dhivesh.sadhwani@aca8d16e-0fc6-11de-ab22-55a8a853a75f>
2009-05-28 19:05:00 +0000

    Logo del wrap con trasparencia

--- CakeChuff/src/cc/simulation/elements/Packet.java
+++ CakeChuff/src/cc/simulation/elements/Packet.java
@@ -7,9 +7,13 @@ import cc.simulation.utils.ModelLoader;
 import com.jme.bounding.BoundingBox;
 import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
+import com.jme.renderer.Renderer;
 import com.jme.scene.Node;
 import com.jme.scene.Spatial;
 import com.jme.scene.shape.Box;
+import com.jme.scene.state.BlendState;
+import com.jme.scene.state.MaterialState;
+import com.jme.system.DisplaySystem;
 /**
  * Implementation and definition of the packet, one of
  * the simulation elements that compose CakeChuff system
@@ -58,7 +62,40 @@ public class Packet extends Node {
 	 */
 	private void loadModel(){
 		URL model=getClass().getClassLoader().getResource("model/wrap.obj");
-		pivot.attachChild(ModelLoader.loadOBJ(model, false));
+		Spatial loadedModel = ModelLoader.loadOBJ(model, false);
+		
+		float opacityAmount = 0.6f;
+		// the sphere material taht will be modified to make the sphere
+		// look opaque then transparent then opaque and so on
+		MaterialState materialState = DisplaySystem.getDisplaySystem().getRenderer()
+				.createMaterialState();
+		materialState
+				.setDiffuse(new ColorRGBA(1f, 1f, 1f, opacityAmount));
+		materialState.setEnabled(true);
+
+		// IMPORTANT: this is used to handle the internal sphere faces when
+		// setting them to transparent, try commenting this line to see what
+		// happens
+		materialState.setMaterialFace(MaterialState.MaterialFace.FrontAndBack);
+
+		loadedModel.setRenderState(materialState);
+		loadedModel.updateRenderState();
+
+		BlendState alphaState = DisplaySystem.getDisplaySystem().getRenderer().createBlendState();
+		alphaState.setBlendEnabled(true);
+		alphaState.setSourceFunction(BlendState.SourceFunction.SourceAlpha);
+		alphaState
+				.setDestinationFunction(BlendState.DestinationFunction.OneMinusSourceAlpha);
+		alphaState.setTestEnabled(true);
+		alphaState.setTestFunction(BlendState.TestFunction.GreaterThan);
+		alphaState.setEnabled(true);
+
+		loadedModel.setRenderState(alphaState);
+		loadedModel.updateRenderState();
+
+		loadedModel.setRenderQueueMode(Renderer.QUEUE_TRANSPARENT);
+		
+		pivot.attachChild(loadedModel);
 //		URL model2=getClass().getClassLoader().getResource("model/blister.obj");
 //		Spatial aux = ModelLoader.loadOBJ(model2);
 //		aux.setLocalScale(new Vector3f(0,-1,0));
